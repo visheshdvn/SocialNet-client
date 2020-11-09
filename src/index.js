690d25ed799ad1a36451baf812004f4a7a79797d
visheshdvn <vishesh.dh1@gmail.com>
2020-11-09 16:00:39 +0530

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from 'axios';
+// import axios from 'axios';
 
 // axios.defaults.baseURL = 'https://socialnetworkbackend.herokuapp.com';
 
--- src/utils/SetAuthToken.js
+++ src/utils/SetAuthToken.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const setAuthToken = token => {
+    axios.defaults.baseURL = "https://socialnetworkbackend.herokuapp.com"
+    
     if (token) {
         axios.defaults.headers.common['x-auth-token'] = token
     } else {
